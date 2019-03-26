95db6bcaf023d271ad69c3ff96497ec414a5ae22
hhyue <hhyue@ctrip.com>
2019-03-26 15:08:16 +0800

    logger print function already has trailing newline, so we remove xxxln function from interface

--- logger.go
+++ logger.go
@@ -3,14 +3,10 @@ package stdlogger
 type LeveledLogger interface {
 	Debugf(format string, v ...interface{})
 	Debug(v ...interface{})
-	Debugln(v ...interface{})
 	Infof(format string, v ...interface{})
 	Info(v ...interface{})
-	Infoln(v ...interface{})
 	Warnf(format string, v ...interface{})
 	Warn(v ...interface{})
-	Warnln(v ...interface{})
 	Errorf(format string, v ...interface{})
 	Error(v ...interface{})
-	Errorln(v ...interface{})
 }
